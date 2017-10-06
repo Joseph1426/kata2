da02f38d3a3bcaa478a7281c4f811d00dd0d163d
Usuario <Usuario@Usuario-PC>
2017-10-06 12:22:26 +0100

    Kata2, histograma versión 5

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -1,4 +1,3 @@
-
 package kata2;
 
 import java.util.HashMap;
@@ -7,13 +6,11 @@ import java.util.Map;
 public class Kata2 {
 
     public static void main(String[] args) {
-        int [] data = {1, 1, 1, 0, 0, 1, 2, 2, 2, 3};
+        String [] data = {"pedro", "rosa", "rosa", "rosa", "luis", "luis"};
         Histogram histo = new Histogram(data);
-        Map<Integer, Integer> histogr =histo.getHistogram();
+        Map<String, Integer> histogr =histo.getHistogram();
         
-       
-       
-        for (int key : histogr.keySet ()){
+        for (String key : histogr.keySet ()){
             System.out.println(key + " ==> " + histogr.get(key));
         }
     }
